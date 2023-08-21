99961e082773ab22bf958a4bf99fa39d116d2a64
taimourafzal <taimoor.afzal@arkhitech.com>
2023-08-21 13:21:45 +0500

    color

--- lua/color.lua
+++ lua/color.lua
@@ -1,1 +1,27 @@
+vim.g.everforest_diagnostic_line_highlight = 1
 vim.cmd('colorscheme everforest')
+
+vim.fn.sign_define('DiagnosticSignError', {
+    text = 'E',
+    texthl = 'DiagnosticSignError',
+    linehl = 'ErrorLine'
+})
+
+vim.fn.sign_define('DiagnosticSignWarn', {
+    text = 'W',
+    texthl = 'DiagnosticSignWarn',
+    linehl = 'WarningLine'
+})
+
+vim.fn.sign_define('DiagnosticSignInfo', {
+    text = 'I',
+    texthl = 'DiagnosticSignInfo',
+    linehl = 'InfoLine'
+})
+
+vim.fn.sign_define('DiagnosticSignHint', {
+    text = 'H',
+    texthl = 'DiagnosticSignHint',
+    linehl = 'HintLine'
+})
+
